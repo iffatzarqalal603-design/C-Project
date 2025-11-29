eb0d8f83af7d8010dd3e51b4386af96487463f77
iffatzarqalal603-design <iffatzarqalal603@gmail.com>
2025-11-29 18:39:14 +0700

    Clean up README.cs by removing unused code
    Removed age and status input validation and pricing logic.

--- README.cs
+++ README.cs
@@ -1,35 +1,4 @@
 # C-Project
-FIndmy C# Project in here !
-Using System ;
-Console.Write("Masukkan umur: ");
-if (!int.TryParse(Console.ReadLine(), out int umur))
-{
-    Console.WriteLine("Input tidak valid");
-    return;
-}
+Findmy C# Project in here !
 
-Console.Write("Masukkan status (1=member, 0=non-member): ");
-if (!int.TryParse(Console.ReadLine(), out int status))
-{
-    Console.WriteLine("Input tidak valid");
-    return;
-}
-
-if (status != 0 && status != 1)
-{
-    Console.WriteLine("Input tidak valid");
-    return;
-}
-
-int harga;
-if (umur < 12)
-{
-    harga = (status == 1) ? 20 : 25;
-}
-else
-{
-    harga = (status == 1) ? 40 : 50;
-}
-
-Console.WriteLine(harga);
 
